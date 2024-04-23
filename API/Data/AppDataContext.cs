c301e83490b0a3aedc0fc1ebe88e5d8ce442c17b
Guilherme <guilhermegarap@gmail.com>
2024-04-23 10:04:40 -0300

    Classe Paciente criada e AppDataContext configurada

---
+++ API/Data/AppDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data;
+
+public class AppDataContext : DbContext
+{
+    public AppDataContext(DbContextOptions<AppDataContext> options) : base(options) { }
+
+    //Classes que vão virar tabelas no banco de dados
+    public DbSet<Paciente> Pacientes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ API/Models/Paciente.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Models;
+
+public class Paciente
+{
+    public int PacienteId { get; set; }
+    public string? PacienteCpf { get; set; }
+    public string? PacienteNome { get; set; }
+    public string? PacienteData { get; set; }
+    public string? PacienteGenero { get; set; }
+    public string? PacienteTelefone { get; set; }
+    public string? PacienteNotas { get; set; }
+
+}
